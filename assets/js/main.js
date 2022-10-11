322222ee9b5177f42b794f58cae9db65c448b57e
matteoga95 <gallo.work95@gmail.com>
2022-10-11 15:11:29 +0200

    aggiunta style su schermo all'es

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,28 +33,78 @@ if (trovato === false) {
 
     console.log("Hai inserito una mail non valida, ricarica la pagina per riprovare...");
 
+    //faccio un alert per dire che la mail era sbagliata e di ricaricare la pagina
+    alert("Hai inserito una mail non valida, ricarica la pagina per riprovare...");
+
+    document.getElementById("blocco-info").style.display = "none";
+
+
 } else {
+
     console.log("Mail corretta, che il gioco dei dadi abbia inizio!!");
+
+    //faccio un alert per dire che la mail era corretta e di continuare
+    alert("Mail corretta, che il gioco dei dadi abbia inizio!!");
+
+
     //vado avanti con il gioco dei dadi
 
-    //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+    //vado avanti solo quando viene cliccato inizia
+    const mainButton = document.querySelector("button");
 
-    const playerNum = Math.floor(Math.random() * 6);
+    mainButton.addEventListener("click", function () {
 
-    const computerNum = Math.floor(Math.random() * 6);
+        //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-    console.log("Il tuo numero è " + playerNum);
+        const playerNum = Math.floor(Math.random() * 6);
 
-    console.log("Mentre il mio è " + computerNum);
+        const computerNum = Math.floor(Math.random() * 6);
 
-    if (playerNum > computerNum) {
-        console.log("Wow che fortuna hai vinto, la prossima volta vincerò io...");
-    } else if (playerNum < computerNum) {
-        console.log("Ho vinto io mi dispiace, la prossima volta forse sarai più fortunato...");
-    } else {
-        console.log("Sembra che qui abbiamo un pareggio, facciamo che in questi casi vinco io?...");
-    }
 
-    console.log("Ricarica la pagina per iniziare da capo.");
+        //abilito a schermo il div con i risultati del gioco
+        document.getElementById("blocco-game").style.display = "block";
+
+
+        //scrivo a console e a schermo il numero del giocatore
+        console.log("Il tuo numero è " + playerNum);
+        document.getElementById("player-number").innerHTML = playerNum;
+
+        //scrivo a console e a schermo il numero del computer
+        console.log("Mentre il mio è " + computerNum);
+        document.getElementById("computer-number").innerHTML = computerNum;
+
+        let resultString = "";
+
+        //se il numero del player è più alto ha vinto lui
+        if (playerNum > computerNum) {
+
+            //valorizzo  il risultato in questo caso
+            resultString = "Wow che fortuna hai vinto, la prossima volta vincerò io..."
+
+            //se il numero del player è più basso ho vinto io
+        } else if (playerNum < computerNum) {
+
+            //valorizzo  il risultato in questo caso
+            resultString = "Ho vinto io mi dispiace, la prossima volta forse sarai più fortunato...";
+
+            //se il numero del player è uguale al mio ho vinto io di nuovo? mah sì dai
+        } else {
+
+            //valorizzo  il risultato in questo caso
+            resultString = "Sembra che qui abbiamo un pareggio, facciamo che in questi casi vinco io?...";
+        }
+
+        //stampo risultato
+        console.log(resultString);
+        document.getElementById("result-String").innerHTML = resultString;
+
+
+    })
+
 
 }
+
+
+//stampo messaggio per far ricominciare l'utente
+console.log("Ricarica la pagina per iniziare da capo.");
+document.getElementById("ricarica").innerHTML = "Ricarica la pagina per iniziare da capo.";
